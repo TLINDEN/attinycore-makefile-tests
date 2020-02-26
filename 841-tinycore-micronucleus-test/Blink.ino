fa98e283a3b62c4cccc539107f5f5d965d288358
Thomas von Dein <tom@izb.net>
2020-02-26 21:24:12 +0100

    +serial

--- 841-tinycore-micronucleus-test/Blink.ino
+++ 841-tinycore-micronucleus-test/Blink.ino
@@ -4,6 +4,21 @@
   This example code is in the public domain.
  */
 
+/*
+  Nanite841 Pinout from above:
+
+         button
+    PA5          PA6
+    PA4          PA7
+    PA3          PB2
+RX0 PA2          PB2
+TX0 PA1          PB1
+    PA0          PB0
+    GND          VCC
+          usb
+
+ */
+
 #define LED LED_BUILTIN
 
 void blinken(int howlong) {
@@ -14,36 +29,37 @@ void blinken(int howlong) {
 }
 
 void lang() {
-  blinken(1000);
+  blinken(500);
 }
 
 void kurz() {
-  blinken(500);
+  blinken(250);
 }
 
 void sos() {
   int i=0;
 
+  Serial.println("long");
   for(i=0; i<3; i++)
     lang();
 
+  Serial.println("short");
   for(i=0; i<3; i++)
     kurz();
 
+  Serial.println("long");
  for(i=0; i<3; i++)
     lang();  
 }
 
 void setup() {
   pinMode(LED, OUTPUT);
-  Serial.begin(9600);
-  Serial.print("Press any key: ");
+  Serial.begin(115200);
+  Serial.print("init");
 }
 
 void loop() {
-  if (Serial.available()) {
-    Serial.println("blinked\n");
-  }
+  Serial.println("blinked");
   sos();
   delay(3000);
 }
