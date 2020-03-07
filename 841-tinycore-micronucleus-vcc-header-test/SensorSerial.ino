af3845437e840c32c922e46b4fcbe05b2f721f4a
Thomas von Dein <tom@izb.net>
2020-03-07 21:03:41 +0100

    add

---
+++ 841-tinycore-micronucleus-vcc-header-test/SensorSerial.ino
@@ -0,0 +1,93 @@
+/* -*-c++-*-
+  Blink
+  Turns on an LED on for one second, then off for one second, repeatedly.
+  This example code is in the public domain.
+ */
+
+/*
+  ./hardware/ATTinyCore/avr/cores/tinymodern/core_pins.h
+  Caution: reverse Pinout: PA3 => Pin 3
+
+  Nanite841 Pinout from above:
+
+              button
+SDO/MISO PA5          PA6 SDI/MOSI
+     SCK PA4          PA7
+     CS  PA3          PB2
+     RX0 PA2          PB2
+     TX0 PA1          PB1
+         PA0          PB0
+         GND          VCC
+               usb
+
+  Bosch BME280 Breakout from above pin header left:
+
+    VCC
+    GND
+    SCL  => SCK
+    SDI  => MOSI
+    CSB  => PA3/3
+    SDO  => MISO
+  
+ */
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include "841vcc.h"
+
+#define NODE_ID       1
+#define OUTPUT_PIN    7
+#define LED           2 //LED_BUILTIN  // PORTB2
+#define CS            3
+#define DELAY         2000
+
+#define SET_OUTPUT(pin) DDRB  |=  (1 << pin)
+#define SET_HIGH(pin)   PORTB |=  (1 << pin)
+#define SET_LOW(pin)    PORTB &= ~(1 << pin)
+
+void print_asufloat(uint32_t val, uint16_t factor) {
+  Serial.print(val / factor);
+  Serial.print(".");
+  Serial.print(val % factor);
+}
+
+void print_asifloat(int32_t val, uint16_t factor) {
+  Serial.print(val / factor);
+  Serial.print(".");
+  Serial.print(val % factor);
+}
+
+void print_measurements() {
+  float vcc;
+  
+  SET_LOW(LED);
+
+  adc_enable();
+  adc_start();
+  vcc = adc_get_vcc();
+  adc_disable();
+  
+  Serial.print("Voltage: ");
+  Serial.println(vcc);
+  
+  SET_HIGH(LED);
+}
+
+void halt() {
+  while(1);
+}
+
+
+void setup() {
+  SET_OUTPUT(LED);
+  Serial.begin(115200);
+  Serial.println("init");
+
+  adc_setup_vcc_measurement();
+}
+
+void loop() {
+  print_measurements();
+  delay(DELAY);
+}
+
---
+++ mylibs/841vcc.h
@@ -0,0 +1,47 @@
+/*
+  Macros for easier usage of vcc measurements.
+  For attiny8441 only.
+
+  We use REFS=0 to select VCC as Reference. i.e.  ADMUXB = 0 << REFS0
+  We then use channel #13 to read the BandGap.  i.e. ADMUXA = 13 << MUX0
+
+  And calculate the value of VCC from the reading.  i.e. ADCW / 1024 = 1.1 / VCC
+  Rearrange i.e. VCC = 1.1 * 1024 / ADCW
+
+  Thanks for the help of the guys at AVR-Freaks:
+  https://www.avrfreaks.net/forum/measuring-vcc-attiny841-using-adc-and-internal-bandgap
+*/
+
+
+#include <util/delay.h>
+
+#ifndef VCC_8441_H
+#define VCC_8441_H
+
+// disable ADC globally
+#define adc_disable() ADCSRA &= ~(1<<ADEN)
+
+// enable
+#define adc_enable()  ADCSRA  =  (1<<ADEN);  _delay_ms(10);
+
+// use supply voltage VCC as reference
+// measure internal bandgap 1v1 voltage reference
+// via channel 13, see table 16-3
+// and do not left shift result
+#define adc_setup_vcc_measurement() \
+  ADMUXB = (0<<REFS0);              \
+  ADMUXA = (13<<MUX0);              \
+  ADCSRB = (0<<ADLAR);
+
+// start measurement
+// and wait till complete
+#define adc_start()      \
+  ADCSRA |= _BV( ADSC ); \
+  while( ( (ADCSRA & (1<<ADSC)) != 0 ) );
+
+// return measured voltage
+// >> 1.1 * 1024 / 225 #=> 5.006222222222223
+// >> 1.1 * 1024 / 341 #=> 3.3032258064516133
+#define adc_get_vcc() 1.1 * 1024 / ADCW;
+
+#endif
