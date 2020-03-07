ef00003cd02cdfb3e3f9007dad9fb721120b9495
Thomas von Dein <tom@izb.net>
2020-03-07 15:17:26 +0100

    fixes

--- 841-tinycore-micronucleus-vcc-test/SensorSerial.ino
+++ 841-tinycore-micronucleus-vcc-test/SensorSerial.ino
@@ -32,22 +32,13 @@ SDO/MISO PA5          PA6 SDI/MOSI
  */
 
 #include <avr/io.h>
-
-#define TINY_BME280_SPI
-#include <TinyBME280.h>
-
-// https://andreasrohner.at/posts/Electronics/New-Arduino-library-for-433-Mhz-AM-Radio-Modules/
-#include <RFTransmitter.h>
+#include <util/delay.h>
 
 #define NODE_ID       1
 #define OUTPUT_PIN    7
 #define LED           2 //LED_BUILTIN  // PORTB2
 #define CS            3
-#define DELAY         1000
-
-static tiny::BME280 sensor;
-RFTransmitter transmitter(OUTPUT_PIN, NODE_ID);
-const long InternalReferenceVoltage = 1083L;
+#define DELAY         2000
 
 #define SET_OUTPUT(pin) DDRB  |=  (1 << pin)
 #define SET_HIGH(pin)   PORTB |=  (1 << pin)
@@ -66,41 +57,22 @@ void print_asifloat(int32_t val, uint16_t factor) {
 }
 
 void print_measurements() {
-  uint32_t pres, humidity;
-  int32_t  temp;
-  int vcc;
-
-  temp     = sensor.readFixedTempC();
-  humidity = sensor.readFixedHumidity();
-  pres     = sensor.readFixedPressure();
-
+  float vcc;
+  
   SET_LOW(LED);
 
-  Serial.print("     Temperature: ");
-  print_asifloat(temp, 100);
-  Serial.println(" Grad C");
+  ADCSRA = (1 << ADEN);                   // enable ADC
+  _delay_ms(10);                          // give it some time to settle
+  ADCSRA |= _BV( ADSC );                  // start measuring
+  while( ( (ADCSRA & (1<<ADSC)) != 0 ) ); // till it's completed
+  // >> 1.1 * 1024 / 225 #=> 5.006222222222223
+  // >> 1.1 * 1024 / 341 #=> 3.3032258064516133
+  vcc = 1.1 * 1024 / ADCW;                // read actual measurement
+  //  ADCSRA &= ~(1<<ADEN);                   // we're done, disable ADC
 
-  Serial.print("        Pressure: ");
-  print_asufloat(pres, 100);
-  Serial.println(" hPa");
-
-  Serial.print("        Humidity: ");
-  print_asufloat(humidity, 1000);
-  Serial.println(" %");
-
-  
-  ADCSRA |= _BV( ADSC );
-  while( ( (ADCSRA & (1<<ADSC)) != 0 ) );
-  //vcc = 1.1 * 1024 / ADCW;
-  vcc = 1024 / ADCW;
-  
-    //  vcc = (((InternalReferenceVoltage * 1024L) / ADC) + 5L) / 10L;
-  Serial.print("         Voltage: ");
+  Serial.print("Voltage: ");
   Serial.println(vcc);
-  Serial.println();
   
-  transmitter.send((byte *)temp, sizeof(temp));
-
   SET_HIGH(LED);
 }
 
@@ -113,17 +85,19 @@ void setup() {
   SET_OUTPUT(LED);
   Serial.begin(115200);
   Serial.println("init");
-  // Enable ADC
-  ADMUXB |= (0<<REFS0);
-  ADMUXA |= (13<<MUX0);
-  ADCSRB |= (0<<ADLAR);
-  ADCSRA |= (1 << ADEN);
-  //analogReference( INTERNAL );
 
-  if(sensor.beginSPI(CS) == false) {
-    Serial.println("Sensor BME280 connect failed, check wiring!");
-    halt();
-  }  
+  // Setup the ADC
+  // https://www.avrfreaks.net/forum/measuring-vcc-attiny841-using-adc-and-internal-bandgap#comment-2862601
+  ADMUXB = (0<<REFS0);  // enable REFS0 bit == internal 1.1 bandgap
+  ADMUXA = (13<<MUX0);  // use channel #13 to read the bandgap (binary 1101, see table 16-3)
+  ADCSRB = (0<<ADLAR);  // do not left shift result
+
+  // disable ADC for powersaving
+  //  ADCSRA &= ~(1<<ADEN);
+
+  // disable analog comperators for powersaving
+  ACSR0A |= _BV(ACD0);
+  ACSR1A |= _BV(ACD1);
 }
 
 void loop() {
